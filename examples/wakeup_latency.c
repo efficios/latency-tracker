e3793d1042dff6b1044862ff36caf306501d3bf7
Julien Desfossez <jdesfossez@efficios.com>
2015-02-28 15:55:06 -0500

    Allow user-space programs to wait on wakeup latency events
    A new /proc/wake_latency file is created, when a latency event is
    generated the readers waiting to read/poll on this file are woken up so
    they can take an action when the high latency occurs.
    An example use-case is provided in watch-wake-latency-event.sh where we
    record an LTTng snapshot everytime a high latency is detected.
    For now a hard-coded rate-limiter of 1 wakeup/second is in place, we'll
    see if we keep it there.
    
    Signed-off-by: Julien Desfossez <jdesfossez@efficios.com>

--- examples/wakeup_latency.c
+++ examples/wakeup_latency.c
@@ -39,6 +39,9 @@
 #include <linux/dcache.h>
 #include <linux/fs.h>
 #include <linux/sched.h>
+#include <linux/proc_fs.h>
+#include <linux/poll.h>
+#include <linux/irq_work.h>
 #include "wakeup_latency.h"
 #include "../latency_tracker.h"
 #include "../wrapper/tracepoint.h"
@@ -72,9 +75,37 @@ struct schedkey {
 	pid_t pid;
 } __attribute__((__packed__));
 
+enum wake_reason {
+	SCHED_TRACKER_WAKE_DATA = 0,
+	SCHED_TRACKER_WAIT = 1,
+	SCHED_TRACKER_HUP = 2,
+};
+
+struct wakeup_tracker {
+	u64 last_alert_ts;
+	u64 ns_rate_limit;
+	wait_queue_head_t read_wait;
+	enum wake_reason reason;
+	bool got_alert;
+	int readers;
+	struct irq_work w_irq;
+};
+
 static struct latency_tracker *tracker;
 
 static int cnt = 0;
+
+static struct proc_dir_entry *wakeup_tracker_proc_dentry;
+static const struct file_operations wakeup_tracker_fops;
+
+void irq_wake(struct irq_work *entry)
+{
+	struct wakeup_tracker *wakeup_priv = container_of(entry,
+			struct wakeup_tracker, w_irq);
+	wakeup_priv->reason = SCHED_TRACKER_WAKE_DATA;
+	wake_up_interruptible(&wakeup_priv->read_wait);
+	wakeup_priv->got_alert = true;
+}
 
 static
 void wakeup_cb(unsigned long ptr)
@@ -82,10 +113,16 @@ void wakeup_cb(unsigned long ptr)
 	struct latency_tracker_event *data =
 		(struct latency_tracker_event *) ptr;
 	struct schedkey *key = (struct schedkey *) data->tkey.key;
+	struct wakeup_tracker *wakeup_priv =
+		(struct wakeup_tracker *) data->priv;
 	struct task_struct *p;
 	u64 delay;
 
 	if (data->cb_flag != LATENCY_TRACKER_CB_NORMAL)
+		return;
+	/* Rate limiter */
+	if ((data->end_ts - wakeup_priv->last_alert_ts) <
+			wakeup_priv->ns_rate_limit)
 		return;
 
 	delay = (data->end_ts - data->start_ts) / 1000;
@@ -96,15 +133,23 @@ void wakeup_cb(unsigned long ptr)
 	rcu_read_lock();
 	p = pid_task(find_vpid(key->pid), PIDTYPE_PID);
 	if (!p)
-		goto end;
+		goto end_unlock;
 	trace_wakeup_latency(key->pid, data->end_ts - data->start_ts,
 			data->cb_flag);
 	printk("wakeup_latency: (%d) %s (%d), %llu us\n", data->cb_flag,
 			p->comm, key->pid, delay);
+	rcu_read_unlock();
 	cnt++;
 
-end:
+	if (wakeup_priv->readers > 0)
+		irq_work_queue(&wakeup_priv->w_irq);
+	wakeup_priv->last_alert_ts = data->end_ts;
+	goto end;
+
+end_unlock:
 	rcu_read_unlock();
+end:
+	return;
 }
 
 static
@@ -131,7 +176,7 @@ void probe_sched_wakeup(void *ignore, struct task_struct *p, int success)
 
 	ret = latency_tracker_event_in(tracker, &key, sizeof(key),
 		thresh, wakeup_cb, timeout, 1,
-		NULL);
+		latency_tracker_get_priv(tracker));
 	if (ret == LATENCY_TRACKER_FULL) {
 //		printk("latency_tracker sched: no more free events, consider "
 //				"increasing the max_events parameter\n");
@@ -155,15 +200,93 @@ void probe_sched_switch(void *ignore, struct task_struct *prev,
 	latency_tracker_event_out(tracker, &key, sizeof(key), 0);
 }
 
+unsigned int tracker_proc_poll(struct file *filp,
+		poll_table *wait)
+{
+	struct wakeup_tracker *wakeup_priv = filp->private_data;
+	unsigned int mask = 0;
+
+	if (filp->f_mode & FMODE_READ) {
+		poll_wait(filp, &wakeup_priv->read_wait, wait);
+		if (wakeup_priv->reason == SCHED_TRACKER_WAKE_DATA)
+			mask |= POLLIN;
+		else
+			mask |= POLLHUP;
+	}
+
+	return mask;
+}
+
+	static
+ssize_t tracker_proc_read(struct file *filp, char __user *buf, size_t n,
+		loff_t *offset)
+{
+	struct wakeup_tracker *wakeup_priv = filp->private_data;
+
+	wait_event_interruptible(wakeup_priv->read_wait,
+			wakeup_priv->got_alert);
+	wakeup_priv->reason = SCHED_TRACKER_WAIT;
+	wakeup_priv->got_alert = false;
+
+	return 0;
+}
+
+	static
+int tracker_proc_open(struct inode *inode, struct file *filp)
+{
+	struct wakeup_tracker *wakeup_priv = PDE_DATA(inode);
+	int ret;
+
+	wakeup_priv->got_alert = false;
+	wakeup_priv->readers++;
+	filp->private_data = wakeup_priv;
+	ret = try_module_get(THIS_MODULE);
+	if (!ret)
+		return -1;
+
+	return 0;
+}
+
+	static
+int tracker_proc_release(struct inode *inode, struct file *filp)
+{
+	struct wakeup_tracker *wakeup_priv = filp->private_data;
+
+	wakeup_priv->readers--;
+	module_put(THIS_MODULE);
+	return 0;
+}
+
+static const
+struct file_operations wakeup_tracker_fops = {
+	.owner = THIS_MODULE,
+	.open = tracker_proc_open,
+	.read = tracker_proc_read,
+	.release = tracker_proc_release,
+	.poll = tracker_proc_poll,
+};
+
 static
 int __init wakeup_latency_init(void)
 {
 	int ret;
+	struct wakeup_tracker *wakeup_priv;
+
+	wakeup_priv = kzalloc(sizeof(struct wakeup_tracker), GFP_KERNEL);
+	if (!wakeup_priv) {
+		ret = -ENOMEM;
+		goto end;
+	}
+	wakeup_priv->reason = SCHED_TRACKER_WAIT;
+	/* limit to 1 evt/sec */
+	wakeup_priv->ns_rate_limit = 1000000000;
 
 	tracker = latency_tracker_create(NULL, NULL, 200, 1000, 100000000, 0,
-			NULL);
+			wakeup_priv);
 	if (!tracker)
 		goto error;
+
+	init_irq_work(&wakeup_priv->w_irq, irq_wake);
 
 	ret = lttng_wrapper_tracepoint_probe_register("sched_wakeup",
 			probe_sched_wakeup, NULL);
@@ -172,6 +295,16 @@ int __init wakeup_latency_init(void)
 	ret = lttng_wrapper_tracepoint_probe_register("sched_switch",
 			probe_sched_switch, NULL);
 	WARN_ON(ret);
+
+	wakeup_tracker_proc_dentry = proc_create_data("wake_latency",
+			S_IRUSR, NULL, &wakeup_tracker_fops, wakeup_priv);
+
+	if (!wakeup_tracker_proc_dentry) {
+		printk(KERN_ERR "Error creating tracker control file\n");
+		ret = -ENOMEM;
+		goto end;
+	}
+	init_waitqueue_head(&wakeup_priv->read_wait);
 
 	ret = 0;
 	goto end;
@@ -187,6 +320,7 @@ static
 void __exit wakeup_latency_exit(void)
 {
 	uint64_t skipped;
+	struct wakeup_tracker *wakeup_priv;
 
 	lttng_wrapper_tracepoint_probe_unregister("sched_wakeup",
 			probe_sched_wakeup, NULL);
@@ -194,9 +328,14 @@ void __exit wakeup_latency_exit(void)
 			probe_sched_switch, NULL);
 	tracepoint_synchronize_unregister();
 	skipped = latency_tracker_skipped_count(tracker);
+	wakeup_priv = latency_tracker_get_priv(tracker);
+	irq_work_sync(&wakeup_priv->w_irq);
+	kfree(wakeup_priv);
 	latency_tracker_destroy(tracker);
+	if (wakeup_tracker_proc_dentry)
+		remove_proc_entry("wake_latency", NULL);
 	printk("Missed events : %llu\n", skipped);
-	printk("Total sched alerts : %d\n", cnt);
+	printk("Total wakeup alerts : %d\n", cnt);
 }
 module_exit(wakeup_latency_exit);
 
---
+++ examples/watch-wake-latency-event.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#lttng-sessiond --extra-kmod-probes=latency_tracker -d
+lttng create --snapshot
+lttng enable-channel -k chan1 --subbuf-size 8M
+lttng enable-event -k -a -c chan1
+lttng start
+
+while true; do
+	cat /proc/wake_latency
+	lttng snapshot record
+done
+
+lttng stop
+lttng destroy
