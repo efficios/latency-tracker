e3c61403266fc4154d64d30023fc10e45466b9fc
Julien Desfossez <jdesfossez@efficios.com>
2015-03-17 11:07:01 -0400

    clean exit ftrace script
    Signed-off-by: Julien Desfossez <jdesfossez@efficios.com>

--- examples/ftrace.sh
+++ examples/ftrace.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
+destroy()
+{
+	echo 0 >/sys/kernel/debug/tracing/tracing_on
+	echo 0  >/sys/kernel/debug/tracing/events/latency_tracker/syscall_latency_stack/enable
+	echo 0  >/sys/kernel/debug/tracing/events/latency_tracker/syscall_latency/enable
+	cat /sys/kernel/debug/tracing/trace_pipe >/dev/null
+	exit 0
+}
+
+trap "destroy" SIGINT SIGTERM
 
 echo 1  >/sys/kernel/debug/tracing/events/latency_tracker/syscall_latency_stack/enable
 echo 1  >/sys/kernel/debug/tracing/events/latency_tracker/syscall_latency/enable 
