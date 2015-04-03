e6d89d5d8ee1c154d7a5ea7d347961baa95eab63
Julien Desfossez <jdesfossez@efficios.com>
2015-04-03 00:01:43 -0400

    example script to use the offcpu with ftrace
    Signed-off-by: Julien Desfossez <jdesfossez@efficios.com>

---
+++ examples/ftrace-offcpu.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+destroy()
+{
+	echo 0 >/sys/kernel/debug/tracing/tracing_on
+	echo 0  >/sys/kernel/debug/tracing/events/latency_tracker/offcpu_sched_switch/enable
+	echo 0  >/sys/kernel/debug/tracing/events/latency_tracker/offcpu_sched_wakeup/enable
+	cat /sys/kernel/debug/tracing/trace_pipe >/dev/null
+	exit 0
+}
+
+trap "destroy" SIGINT SIGTERM
+
+echo 1  >/sys/kernel/debug/tracing/events/latency_tracker/offcpu_sched_switch/enable
+echo 1  >/sys/kernel/debug/tracing/events/latency_tracker/offcpu_sched_wakeup/enable
+echo 1  >/sys/kernel/debug/tracing/tracing_on 
+cat /sys/kernel/debug/tracing/trace_pipe
