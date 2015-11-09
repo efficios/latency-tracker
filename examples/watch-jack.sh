3bc2b0ac70e920f63692072926e72197e39eea1f
Julien Desfossez <jdesfossez@efficios.com>
2015-11-08 23:00:24 -0500

    fix watch jackd script
    Signed-off-by: Julien Desfossez <jdesfossez@efficios.com>

--- examples/watch-jack.sh
+++ examples/watch-jack.sh
@@ -8,6 +8,11 @@ destroy()
 	exit 0
 }
 
+if test $UID != 0; then
+	echo "Need to be root"
+	exit -1
+fi
+
 trap "destroy" SIGINT SIGTERM SIGPIPE SIGHUP
 
 chrt -r -p 99 $$
@@ -15,6 +20,7 @@ chrt -r -p 99 $(pgrep lttng-sessiond)
 chrt -r -p 99 $(pgrep lttng-consumerd)
 
 rm -rf /tmp/debugrt
+pgrep -u root lttng-sessiond || lttng-sessiond -d
 lttng create --snapshot -o /tmp/debugrt
 lttng enable-channel -k bla --subbuf-size 2M
 lttng enable-event -k -a -c bla
@@ -24,6 +30,8 @@ lttng start
 echo 25000000 > $CONF/threshold
 echo 0 > $CONF/filters/timer_tracing
 echo 1 > $CONF/filters/irq_tracing
+echo -1 > $CONF/filters/irq_filter
+echo -1 > $CONF/filters/softirq_filter
 echo 1 > $CONF/filters/enter_userspace
 echo 0 > $CONF/filters/switch_out_blocked
 echo jackd > $CONF/filters/procname
