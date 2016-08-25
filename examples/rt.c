25e52aeb7f4bb3bb116161f37e171434ea19cf80
Julien Desfossez <jdesfossez@efficios.com>
2016-08-25 12:10:01 -0400

    Fix the potentially wrong branch ref taking
    Signed-off-by: Julien Desfossez <jdesfossez@efficios.com>

--- examples/rt.c
+++ examples/rt.c
@@ -635,16 +635,16 @@ struct latency_tracker_event *event_transition(void *key_in, int key_in_len,
 	if (branch) {
 		struct event_data *data_root;
 
-		ret = _latency_tracker_get_event(event_in);
+		/* First branch, the others get the pointer from the memcpy */
+		if (!data_in->root) {
+			data_out->root = event_in;
+		}
+		ret = _latency_tracker_get_event(data_out->root);
 		if (!ret) {
 			printk("ERR _latency_tracker_get_event\n");
 			latency_tracker_put_event(event_out);
 			event_out = NULL;
 			goto end_del;
-		}
-		/* First branch, the others get the pointer from the memcpy */
-		if (!data_in->root) {
-			data_out->root = event_in;
 		}
 		/*
 		 * When branching, we have to reset the good_branch flags
