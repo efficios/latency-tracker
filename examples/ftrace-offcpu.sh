06febd17e24bfa1554685066f8bf372a0f679523
Michael Jeanson <mjeanson@efficios.com>
2016-08-29 16:26:17 -0400

    Add licensing information
    Signed-off-by: Michael Jeanson <mjeanson@efficios.com>

--- examples/ftrace-offcpu.sh
+++ examples/ftrace-offcpu.sh
@@ -1,4 +1,22 @@
-#!/bin/bash
+#!/bin/sh
+#
+# Copyright (C) 2015 Julien Desfossez <jdesfossez@efficios.com>
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; only
+# version 2.1 of the License.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+#
+
 destroy()
 {
 	echo 0 >/sys/kernel/debug/tracing/tracing_on
@@ -12,5 +30,6 @@ trap "destroy" SIGINT SIGTERM SIGPIPE SIGHUP
 
 echo 1  >/sys/kernel/debug/tracing/events/latency_tracker/latency_tracker_offcpu_sched_switch/enable
 echo 1  >/sys/kernel/debug/tracing/events/latency_tracker/latency_tracker_offcpu_sched_wakeup/enable
-echo 1  >/sys/kernel/debug/tracing/tracing_on 
+echo 1  >/sys/kernel/debug/tracing/tracing_on
+
 cat /sys/kernel/debug/tracing/trace_pipe
